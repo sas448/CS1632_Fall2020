31b1b296286a527e6cb3eecb9a317595b73b2bd0
wonsunahn <wahn@pitt.edu>
2020-09-14 08:57:59 -0400

    Update CatScaleTest.java

--- sample_code/junit_cat_scale/CatScaleTest.java
+++ sample_code/junit_cat_scale/CatScaleTest.java
@@ -1,17 +1,17 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.Before;
+
 import org.mockito.*;
 
 public class CatScaleTest {
-    @Test
-    public void testReport() {
-	CatScale scale = new CatScale();
-	Cat cat = Mockito.mock(Cat.class);
-	Mockito.when(cat.getWeight()).thenReturn(80);
-    	assertEquals(scale.report(cat), "Overweight");
-    	Mockito.verify(cat).getWeight();
-    }
+	@Test
+	public void testReport() {
+		CatScale scale = new CatScale();
+		Cat cat = Mockito.mock(Cat.class);
+		Mockito.when(cat.getWeight()).thenReturn(80);
+		assertEquals("Overweight", scale.report(cat));
+		Mockito.verify(cat).getWeight();
+	}
 }
